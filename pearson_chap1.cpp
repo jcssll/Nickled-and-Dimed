05ec3b949bfd0f9eafcee5250823a5c592309bd3
jcssll <essentialnetworker@gmail.com>
2019-09-20 14:50:27 -0400

    Correction to declaration of variabes and values

--- pearson_chap1.cpp
+++ pearson_chap1.cpp
@@ -1,32 +1,37 @@
+// Header File
+
 #include <iostream> 
 using namespace std; 
 
+//Main Function 
  int main()
  {
-	 
+//Decalare my variables
+
+ 	int number_of_quarters, number_of_dimes, number_of_nickels, total;
 	int quarters = .25;
+	int dimes = 10;
+	int nickels = 5;
+	//removing variables I created with values.
+	/*
 	int amtQuart = 2;
-	int dimes = .1; 
 	int amtDimes = 3;
-	int nickels = .05;
 	int amtNickels = 1;
-	int total = (amtQuart * quarters) + (amtDimes * dimes) + (amtNickels * nickels);
+*/
 	 
+//Prompt and read input from the keyboard	 
+	cout << "Enter the number of quarters, ";
+	cin >> number_of_quarters; 
 	 
-	cout << "Enter the number of quarters.";
-	cin >> amtQuart; 
+	cout << "Enter the amount of dimes ";
+	cin >> number_of_dimes; 
 	 
-	cout << "Enter the amount of dimes.";
-	cin >> amtDimes; 
-	 
-	cout << "Enter the amount of nickels.";
-	cin >> amtNickels; 
-	 
-	cout << endl; 
-	 
-	cout << "Coins are worth " << total << " cents" << endl;
-	 
-	 
-	 
+	cout << "Enter the amount of nickels:";
+	cin >> number_of_nickels; 
+
+	total = (number_of_quarters * quarters) + (number_of_dimes * dimes) + (number_of_nickels * nickels);
+	 //result
+	cout << total;
+
  	return 0;
  }
